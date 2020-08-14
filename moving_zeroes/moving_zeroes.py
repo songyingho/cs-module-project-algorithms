c51bb38475891dfd8560f814d258208b5dc30f50
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-08-14 09:46:54 +0800

    completed moving_zeroes.py

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,8 +4,18 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
-
-    pass
+    count = 0
+    temp = []
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            temp.append(arr[i])
+        else:
+            count += 1
+        
+    for j in range(count):
+        temp.append(0)
+    
+    return temp
 
 
 if __name__ == '__main__':
